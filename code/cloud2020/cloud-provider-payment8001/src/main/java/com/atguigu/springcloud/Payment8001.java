feaa9fe283c40bafe223f2be1e3159b1dafaa352
niushuai <niushuai951101@gmail.com>
2021-02-20 18:36:06 +0800

    进度更新

---
+++ code/cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/Payment8001.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ns
+ * @date 2021/2/20
+ */
+@SpringBootApplication
+public class Payment8001 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Payment8001.class, args);
+    }
+}
