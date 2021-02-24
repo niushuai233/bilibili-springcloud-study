26f71634158cbe15888a2481a9752f513678e7e1
niushuai <niushuai951101@gmail.com>
2021-02-24 17:36:20 +0800

    生产者接入注册中心

--- code/cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/Payment8001.java
+++ code/cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/Payment8001.java
@@ -2,11 +2,13 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ns
  * @date 2021/2/20
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class Payment8001 {
 
