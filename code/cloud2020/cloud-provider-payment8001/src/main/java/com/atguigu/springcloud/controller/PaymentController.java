2d190fec7f34d3de08eefe988f746dc6d91eca05
niushuai <niushuai951101@gmail.com>
2021-02-25 15:33:11 +0800

    Update PaymentController.java

--- code/cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ code/cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -1,13 +1,18 @@
 package com.atguigu.springcloud.controller;
 
+import cn.hutool.json.JSONUtil;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaServiceInstance;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author ns
@@ -17,6 +22,9 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Resource
     private PaymentService paymentService;
@@ -52,4 +60,28 @@ public class PaymentController {
 
         return new CommonResult<Payment>(445, "未找到记录");
     }
+
+    @GetMapping("/discovery")
+    public CommonResult discovery() {
+
+        List<String> services = discoveryClient.getServices();
+
+
+        for (String service : services) {
+
+            List<ServiceInstance> instances = discoveryClient.getInstances(service);
+
+            for (ServiceInstance instance : instances) {
+
+                log.info("{} {} {} {} {}", instance.getInstanceId(), instance.getHost(), instance.getPort(), instance.getScheme(), instance.getUri());
+            }
+
+            log.info("service : {} complete", JSONUtil.toJsonStr(service));
+
+        }
+
+
+        return new CommonResult(200, "success", discoveryClient);
+    }
+
 }
