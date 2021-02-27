697834574e1f88fb922b010951a5849dd7152073
niushuai <niushuai951101@gmail.com>
2021-02-27 17:50:00 +0800

    通过消费者服务调用生产者

---
+++ code/cloud2020/cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,38 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.PaymentHystrixService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author ns
+ * @date 2021/2/24
+ */
+@Slf4j
+@RestController
+@RequestMapping("/consumer/payment")
+public class OrderHystrixController {
+
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") String id) {
+
+        return paymentHystrixService.paymentInfo_OK(id) + " from port " + port;
+    }
+
+    @GetMapping("/hystrix/timeout/{id}")
+    public String paymentInfo_Timeout(@PathVariable("id") String id) {
+        return paymentHystrixService.paymentInfo_Timeout(id) + " from port " + port;
+    }
+}
---
+++ code/cloud2020/cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author ns
+ * @date 2021/2/27
+ */
+@Component
+@FeignClient(value = "cloud-provider-hystrix-service")
+public interface PaymentHystrixService {
+
+    @GetMapping("/payment/hystrix/ok/{id}")
+    String paymentInfo_OK(@PathVariable("id") String id);
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    String paymentInfo_Timeout(@PathVariable("id") String id);
+}
