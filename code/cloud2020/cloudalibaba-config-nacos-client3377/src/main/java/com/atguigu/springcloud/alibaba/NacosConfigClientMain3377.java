33ac9948fef0c6a8f2cd6656951780de3db6685c
niushuai <niushuai951101@gmail.com>
2021-03-05 10:55:43 +0800

    nacos 配置中心

--- code/cloud2020/cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/NacosConfigClientMain3377.java
+++ code/cloud2020/cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud.alibaba;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 
 
 @EnableDiscoveryClient
--- code/cloud2020/cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/controller/ConfigClientController.java
+++ code/cloud2020/cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/controller/ConfigClientController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
 @RefreshScope
+@RestController
 public class ConfigClientController
 {
     @Value("${config.info}")
